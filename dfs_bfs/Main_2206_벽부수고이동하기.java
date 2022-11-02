a1dae81e4b55aba03cb0be0241d1c16d7107a39e
leey0818 <leey0818@gmail.com>
2022-11-02 20:05:11 +0900

    7th bfs algo

--- dfs_bfs/Main_2206_벽부수고이동하기.java
+++ dfs_bfs/Main_2206_벽부수고이동하기.java
@@ -13,25 +13,19 @@ public class Main_2206_벽부수고이동하기 {
         String[] arr = br.readLine().trim().split(" ");
         int N = Integer.parseInt(arr[0]);
         int M = Integer.parseInt(arr[1]);
-        int[][] map = new int[N][M];
+        char[][] map = new char[N][M];
 
         for (int h = 0; h < N; h++) {
-            arr = br.readLine().trim().split("");
-            for (int w = 0; w < M; w++) {
-                map[h][w] = Integer.parseInt(arr[w]);
-            }
+            map[h] = br.readLine().toCharArray();
         }
 
         int distance = bfs(map, N, M);
         System.out.println(distance);
     }
 
-    private static int bfs(int[][] map, int N, int M) {
-        int minCount = N * M;
-        boolean isArrived = false;
-
-        boolean[][] visit = new boolean[N][M];
-        visit[0][0] = true;
+    private static int bfs(char[][] map, int N, int M) {
+        boolean[][] visitForNormal = new boolean[N][M];
+        boolean[][] visitForBroken = new boolean[N][M];
 
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{0, 0, 1, 0});
@@ -42,15 +36,9 @@ public class Main_2206_벽부수고이동하기 {
             int y = cur[1];
             int c = cur[2];
 
-            System.out.println(String.format("(%d,%d) %d %d", cur[0], cur[1], cur[2], cur[3]));
-
             if (x + 1 == M && y + 1 == N) {
-                if (c < minCount) minCount = c;
-                isArrived = true;
-                continue;
+                return c;
             }
-
-            // visit[y][x] = true;
 
             for (int d = 0; d < 4; d++) {
                 int nx = x + dw[d];
@@ -59,19 +47,22 @@ public class Main_2206_벽부수고이동하기 {
 
                 if (nx < 0 || ny < 0 || nx >= M || ny >= N) continue;
 
-                if (visit[ny][nx]) continue;
-
-                if (map[ny][nx] != 0) {
-                    if (isBroken) continue;
-                    isBroken = true;
+                // 다음으로 이동할 좌표가 벽이 아님
+                if (map[ny][nx] == '0') {
+                    if (isBroken && !visitForBroken[ny][nx]) {
+                        visitForBroken[ny][nx] = true;
+                        queue.offer(new int[]{nx, ny, c + 1, 1});
+                    } else if (!isBroken && !visitForNormal[ny][nx]) {
+                        visitForNormal[ny][nx] = true;
+                        queue.offer(new int[]{nx, ny, c + 1, 0});
+                    }
+                } else if (!isBroken && !visitForBroken[ny][nx]) {
+                    visitForBroken[ny][nx] = true;
+                    queue.offer(new int[]{nx, ny, c + 1, 1});
                 }
-
-                visit[ny][nx] = true;
-
-                queue.offer(new int[]{nx, ny, c + 1, isBroken ? 1 : 0});
             }
         }
 
-        return isArrived ? minCount : -1;
+        return -1;
     }
 }
